9f4081291efb3416ba74a76b1cc93a0463a0626d
VadimQa <Greench707@gmail.com>
2022-05-03 01:17:35 +0300

    new command

--- lesson1.js
+++ lesson1.js
@@ -4,27 +4,58 @@
 // let yolochka6 = 6;
 // let yolochka_name = " Zelonaya"
 
-// let result = yolochka4 + yolochka6;
-// let result_2 = yolochka6 + yolochka_name;
+// // let result = yolochka4 + yolochka6;
+// // let result_2 = yolochka6 + yolochka_name;
 
 
-// console.log(result);  //команда для вывода результата.
-// console.log(result_2); //в результате произошла конкатенция (сделать сложение с числом и строкой не возможнои)
-                       // по этому оно клеит цыфру и слово.Склейка это конкотенация.
+// // console.log(result);  //команда для вывода результата.
+// // console.log(result_2); //в результате произошла конкатенция (сделать сложение с числом и строкой не возможнои)
+//                        // по этому оно клеит цыфру и слово.Склейка это конкотенация.
 
 
-//Булевый тип данныъ это ложь либо истина 
-let t = true //Эквавалент тру это еденица 
-let f = false //Эквивалент фалсе это ноль 
+// Булевый тип данные это ложь либо истина 
+// let t = true //Эквавалент тру это еденица 
+// let f = false //Эквивалент фалсе это ноль 
+
+// //console.log(t + f) //Если вы складываете ложь и истину то получаеться истина как 1+0.
+
+// if (yolochka4 + yolochka6 === 18) { //если здесь истина то кусок кода ниже выполниться 
+//     let apples = 3+5
+//     console.log(""If true = " + apples")
+// }
+//     else if (yolochka4 + yolochka6 == 11){
+//         let milk = 2
+//         console.log("else if = " + milk)   
+//      }    
+//      else if (yolochka4 + yolochka6 == 10){
+//              let water = 15
+//              console.log("else if 2 = " + water)  
+    
+//      //у конструкции иф есть расширение 
+//  } else {
+//      let meat = 2+2
+//      console.log("If not true = " + meat)
+//  }
+
+// let t = true //Эквавалент тру это еденица 
+// let f = false //Эквивалент фалсе это ноль 
 
 //console.log(t + f) //Если вы складываете ложь и истину то получаеться истина как 1+0.
 
-if (f) { //если здесь истина то кусок кода нижу выполниться 
-    let apples = 3+5
-    console.log("If true = " + apples)
-    //у конструкции иф есть расширение 
-} else {
-    let meat = 2+2
-    console.log("If not true = " + meat)
-}
+let age = 12;
 
+if (age >= 18 && age < 60){
+        console.log("Full conten")
+}   
+    else if (age < 18){
+        console.log("Multiki")
+}
+    else if (age >= 60){
+        console.log("Kultura")
+}
+    else if (age < 0){
+        console.log("Good Luck")
+}    
+    else {
+        console.log("Good Luck!")
+}
