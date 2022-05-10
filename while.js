21037bd53237ef4ae6e552afd21de78b23b6c2d2
VadimQa <Greench707@gmail.com>
2022-05-10 13:35:21 +0300

    new command

---
+++ while.js
@@ -0,0 +1,26 @@
+
+
+//console.log("1 -- " + true || true)
+//console.log(true || falce)
+//console.log(falce || true)
+
+//console.log(true && true)
+//console.log(true && false)
+
+let item_1 = 0
+let item_2 = 20
+//let item_3 = (item_2 > 50)? console.log("IDI Kuwatb") : console.log("Hodi golodniy")
+
+//console.log(item_3)
+
+while(item_1 < 20) {
+    console.log(item_1, "= idi kuhat")
+    item_1++
+}
+
+//20 = 10 + 5 * 2
+//if (item_1 > 3 && item_2 == 15 && item_3 != 50) {
+  //  console.log("IDI Kuwatb")
+//} else {
+   // console.log("Hodi golodniy")
+//}
