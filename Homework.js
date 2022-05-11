048b2c47ac387f634957400d39d0c493f841131f
VadimQa <Greench707@gmail.com>
2022-05-11 20:33:14 +0300

    new command

---
+++ Homework.js
@@ -0,0 +1,26 @@
+// let item_1 = 5
+// console.log(item_1)
+// let item_2 = 3
+// console.log(item_2)
+// let item_3 = item_1 + item_2
+// console.log(item_3)
+// let item_4 = "yolochka"
+// console.log(item_4)
+// console.log(item_3 + item_4)
+// console.log(item_3 * item_4)
+// let item_5 = item_3
+// console.log(item_5)
+// let item_6 = 15
+// let item_6_type = item_6
+// console.log("item_6 ==" , typeof item_6)
+// let item_7 = String(item_6)
+// console.log("item_7 == ", typeof(item_7))
+let age_1 = 10
+let age_2 = 18
+let age_3 = 60
+    if (age_1 < age_2 )
+    console.log("you dont have access cause you age is", age_1, "its less then 18")
+    if (age_1 >= age_2 && age_1 < age_3)
+    console.log("Welcome")
+    if (age_1 > age_3)
+    console.log("keep calm and Culture channel")
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+let item_1 = 5
+// console.log(item_1)
+// let item_2 = 3
+// console.log(item_2)
+// let item_3 = item_1 + item_2
+// console.log(item_3)
+// let item_4 = "yolochka"
+// console.log(item_4)
+// console.log(item_3 + item_4)
+// console.log(item_3 * item_4)
+// let item_5 = item_3
+// console.log(item_5)
+// let item_6 = 15
+// let item_6_type = item_6
+// console.log("item_6 ==" , typeof item_6)
+// let item_7 = String(item_6)
+// console.log("item_7 == ", typeof(item_7))
