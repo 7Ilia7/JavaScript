cd915ae78de7b8f8a0b262bfc3149e4d43d956fd
Illia Demchuk <92856173+7Ilia7@users.noreply.github.com>
2022-09-16 10:46:22 +0300

    Update speed.js

--- speed.js
+++ speed.js
@@ -1,3 +1,7 @@
+console.time('our cicle');
 for (let i = 0; i < 1000; i++){
     console.log(Math.pow(i, 0.5));
 }
+console.timeEnd('our cicle')
+
+//измерение скорости действия скрипта через взятие в блок использую console.time
